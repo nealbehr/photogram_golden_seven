af66d0637c172c3d7d4d224bc825b7ed15f7d8b2
nealbehr <nealbehr@gmail.com>
2015-05-11 15:15:52 -0500

    Completed Homework
    Completed.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,54 @@
 class PhotosController < ApplicationController
-  def index
-    @list_of_photos = Photo.all
+
+  def edit_complete
+   p = Photo.all.find(params[:id])
+   p.source = params[:the_source]
+   p.caption = params[:the_caption]
+   p.save
+
+   redirect_to("http://localhost:3000/photos")
+end
+
+
+def create_row
+     p = Photo.new
+     p.source = params[:the_source]
+     p.caption = params[:the_caption]
+     p.save
+
+     redirect_to("http://localhost:3000/photos")
+end
+
+  def destroy
+     Photo.all.find(params["id"]).destroy
+
+     redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def edit_form
+   @id = params[:id]
+   @specificPhoto = Photo.all.find(@id)
+   render "edit_form"
   end
 
 
+
+
+
+  def index
+    @list_of_photos = Photo.all
+ end
+
+ def show
+   @id = params["id"]
+   @specificPhoto = Photo.all.find(@id)
 end
+
+def new_form
+end
+
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos",               { :controller => "photos", :action => "index" })
+  get("/photos/new",           { :controller => "photos", :action => "new_form" })
+  get("/create_photo",  { :controller => "photos", :action => "create_row" })
+  get("/delete_photo/:id",  { :controller => "photos", :action => "destroy" })
+  get("/photos/:id",           { :controller => "photos", :action => "show" })
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/:id/edit_complete", { :controller => "photos", :action => "edit_complete" })
+
 
 end
+
